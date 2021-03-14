18680d4743b5132e6b0d92a8f3ae84c8d625a1e1
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2021-03-14 19:43:46 +0200

    lombok

--- basic/src/main/java/org/example/basic/service/SpeakerServiceImpl.java
+++ basic/src/main/java/org/example/basic/service/SpeakerServiceImpl.java
@@ -2,21 +2,15 @@ package org.example.basic.service;
 
 import org.example.basic.model.Speaker;
 import org.example.basic.repository.SpeakerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("speakerService")
+@lombok.RequiredArgsConstructor
 public class SpeakerServiceImpl implements SpeakerService {
 
     private final SpeakerRepository repository;
-
-    @Autowired
-    public SpeakerServiceImpl(SpeakerRepository repository) {
-        System.out.println("Args Constructor");
-        this.repository = repository;
-    }
 
     @Override
     public List<Speaker> findAll() {
