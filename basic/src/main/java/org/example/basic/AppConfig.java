9c4a8c850b3348cb970bab1e9b58090811acbe98
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2021-03-14 18:20:39 +0200

    scopes

--- basic/src/main/java/org/example/basic/AppConfig.java
+++ basic/src/main/java/org/example/basic/AppConfig.java
@@ -4,21 +4,25 @@ import org.example.basic.repository.HibernateSpeakerRepositoryImpl;
 import org.example.basic.repository.SpeakerRepository;
 import org.example.basic.service.SpeakerService;
 import org.example.basic.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)      //default value
     public SpeakerService getSpeakerService() {
-        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
-        //service.setRepository(getSpeakerRepository());
-        return service;
+        System.out.println("--getSpeakerService");
+        return new SpeakerServiceImpl(getSpeakerRepository());
     }
 
     @Bean(name = "speakerRepository")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)      //default value
     public SpeakerRepository getSpeakerRepository() {
+        System.out.println("--getSpeakerRepository");
         return new HibernateSpeakerRepositoryImpl();
     }
 
--- basic/src/main/java/org/example/basic/Application.java
+++ basic/src/main/java/org/example/basic/Application.java
@@ -12,5 +12,10 @@ public class Application {
         SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
+
+        SpeakerService service1= applicationContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service1.findAll().get(0).getFirstName());
+
+
     }
 }
