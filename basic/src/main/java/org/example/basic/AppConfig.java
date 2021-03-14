094fd1e5d3a0a1ffbab5afed7dcfaeee8489714b
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2021-03-14 18:04:47 +0200

    setter injection

---
+++ basic/src/main/java/org/example/basic/AppConfig.java
@@ -0,0 +1,26 @@
+package org.example.basic;
+
+import org.example.basic.repository.HibernateSpeakerRepositoryImpl;
+import org.example.basic.repository.SpeakerRepository;
+import org.example.basic.service.SpeakerService;
+import org.example.basic.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService() {
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        service.setRepository(getSpeakerRepository());
+        return service;
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new HibernateSpeakerRepositoryImpl();
+    }
+
+
+}
--- basic/src/main/java/org/example/basic/Application.java
+++ basic/src/main/java/org/example/basic/Application.java
@@ -1,12 +1,15 @@
 package org.example.basic;
 
 import org.example.basic.service.SpeakerService;
-import org.example.basic.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
 
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
